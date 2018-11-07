ca069f2d489dc29059e36a68cbef26e985c634ff
lwsh <helenmary.barrameda@uap.asia>
2018-11-07 18:01:15 +0800

    spaces

--- MachineProblem_Barrameda.R
+++ MachineProblem_Barrameda.R
@@ -1,9 +1,18 @@
 #Define an R function that removes NA values from a vector.
+
+
 #Define an R function that computes the factorial of given an integer argument. The output should be a vector of length 1.
+
 #Define an R function that computes the determinant of a given matrix. The output should be a vector of length 1.
+
 #Define an R function that sorts a given vector in decreasing order. The output should be a vector of the same length. It should accept both numeric or character vectors.
+
 #Define an R function that accepts a Date (POSIXct) as argument and outputs the day of the week as characters. Use modulo operator.
+
 #Create a function to compute for your net pay at work.
+
 #Create a function that accepts a vector and and integer n and returns nth highest number
+
 #Create a function that computes the compound interest of an investment given the rate, time, and initial amount or principal.
+
 #Create a function isPrime(n) that accepts an integer and outputs a Boolean value (TRUE or FALSE) depending whether the integer is a prime number or not.
