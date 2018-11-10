dbc2d48cc3ab02b90914c811583d676be257177d
Helen Mary Labao-Barrameda <helenmarylabao-barrameda@SuburbanMom.local>
2018-11-10 16:10:44 +0800

    before cofactor method

--- MachineProblem_Barrameda.R
+++ MachineProblem_Barrameda.R
@@ -20,8 +20,11 @@ fctr <- function(input) {
 }
 
 fctr(inputtest)
+print(fctr(inputtest))
 
 #Define an R function that computes the determinant of a given matrix. The output should be a vector of length 1.
+
+##Note: Sir Elmer, please Source this, it does not do the interactive prompt when clicking Run only Thank you. 
 
 #Test variables so I can check my computation
 testdata <- matrix(nrow = 3, ncol = 3)
@@ -38,15 +41,21 @@ testdata[3,2]=-4
 testdata[3,3]=1
 print(det(testdata))
 
-#Matrix Writer Function for input matrix
-
 
 #Get the dimension of the matrix first
 SqMtrxDmnsn <-as.numeric(readline(prompt="Dimension of square matrix"))
 
-inputmatrix <- matrix(nrow=SqMtrxDmnsn,ncol=SqMtrxDmnsn)
-
-print(inputmatrix)
+inputmatrix <- matrix(0,nrow=SqMtrxDmnsn,ncol=SqMtrxDmnsn)
+#Initializer that prompts user to enter the elements of the matrix
+#Matrix Writer Function for input matrix
+for(i in 1:SqMtrxDmnsn)
+  {for(j in 1:SqMtrxDmnsn)
+  {
+  inputmatrix[i,j] <- as.numeric(readline(prompt="Enter element (left to right first from top to bottom row 1,1 then 1,2... 1,n, 2,1... 2,n)"))  
+  j=j+1
+  }
+  i=i+1
+  }
 
 detrmntdos <- function(x) {
   #Basic determinant formula
@@ -59,26 +68,25 @@ detrmnttres <- function(x) {
   tres <- x[1,1]*x[2,2]*x[3,3]+x[1,2]*x[2,3]*x[3,1]+x[1,3]*x[2,1]*x[3,2]-x[3,1]*x[2,2]*x[1,3]-x[3,2]*x[2,3]*x[1,1]-x[3,3]*x[2,1]*x[1,2]
   return(tres)
 }
-detrmnttres(testdata)
-
-detrmntkwatro <- function(x) {
-  
-} 
 
 detrmntgeneral <- function(x) {
-  
+  return(det(x))
 }
+
+
+#Checker of my calculations
+det(x)
 
 
 if (SqMtrxDmnsn==2) {
   detrmntdos(inputmatrix)
 } else if (SqMtrxDmnsn==3) {
   detrmnttres(inputmatrix)
-} else if (SqMtrxDmnsn==4) {
-  detrmntkwatro(inputmatrix)
-} else if (SqMtrxDmnsn>4) {
+} else if (SqMtrxDmnsn>3) {
   detrmntgeneral(inputmatrix)
 } else print("Hey, invalid matrix yan, buddy!")
+
+
 
 
 #Define an R function that sorts a given vector in decreasing order. The output should be a vector of the same length. It should accept both numeric or character vectors.
